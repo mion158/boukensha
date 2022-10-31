b73d5e5a760791029e3e391d73fec40982736257
mion158 <111487792+mion158@users.noreply.github.com>
2022-10-31 19:39:03 +0000

    init

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'youwillneverguess'
+app.config['SECRET_KEY'] = 'secretlol'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.app_context().push()
 
-@app.route('/', methods=["GET","POST"])
-def index():
-    return "Hiya"
+db = SQLAlchemy(app)
+
+db.create_all()
+# instantiate LoginManager
+login = LoginManager(app)
+# redirect not logged in users
+login.login_view = 'login'
+
