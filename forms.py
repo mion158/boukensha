0a819b762cbe4de4ace9286f968eb1ad86aa9510
mion158 <111487792+mion158@users.noreply.github.com>
2022-11-01 19:03:23 +0000

    format

--- forms.py
+++ forms.py
@@ -12,11 +12,13 @@ class RegistrationForm(FlaskForm):
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
 
 class DestinationForm(FlaskForm):
     city = StringField('city')
