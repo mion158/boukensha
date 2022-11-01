e98f92a3634534d2bb52bdea35f7e2afb5c00210
mion158 <111487792+mion158@users.noreply.github.com>
2022-11-01 19:01:40 +0000

    removed unused

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, render_template, request
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
